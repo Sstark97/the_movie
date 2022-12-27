63b313f9e8214c4b44a7365401fe0f3564acfebe
Sstark97 <aitorscinfo@gmail.com>
2022-12-27 21:04:41 +0000

    Add Stars component

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,4 +1,5 @@
 import moment from "moment"
+import Stars from "./Stars"
 import type { Movie } from "@customTypes/movies"
 import { IMG_URI } from "../const"
 import "@styles/components/MovieItem.scss"
@@ -8,15 +9,17 @@ const MovieItem = ({ movie }: { movie: Movie }) => {
 
   // Format Date with moment.js and in English Format
   const formatDate = moment(release_date).format("ll")
-  const popularityPercent = popularity >= 5000 ? 5 : (popularity * 5 / 5000).toFixed(2)
+
+  // Format popularity in base to 5 stars like the most
+  const popularityPercent = popularity >= 5000 ? 5 : parseFloat((popularity * 5 / 5000).toFixed(1))
 
   return (
     <li>
       <img src={`${IMG_URI}${poster_path}`} alt="poster" />
       <div className="movie_info">
+        <Stars stars={Math.round(popularityPercent)}/>
         <h3>{original_title}</h3>
         <p>{formatDate}</p>
-        <p>{popularityPercent}</p>
       </div>
     </li>
   )
