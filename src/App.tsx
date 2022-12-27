2a9bddc69c14c4cc98572fe7fa2a0dab7981ffc1
Sstark97 <aitorscinfo@gmail.com>
2022-12-27 19:06:15 +0000

    Get connect with API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react"
 import Layout from "@containers/Layout"
+import { API_MOST_POPULAR } from "./const"
 import "@styles/App.scss"
 
 const App = () => {
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await fetch(API_MOST_POPULAR)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    loadData()
+  }, [])
+
   return (
     <Layout>
       <p>Hola</p>
---
+++ src/const.ts
@@ -0,0 +1,5 @@
+const API_MOST_POPULAR = `${import.meta.env.VITE_API}movie/popular?api_key=${import.meta.env.VITE_API_KEY_AUTH_V3}`
+
+export {
+    API_MOST_POPULAR
+}
