c2fc925f11dcd5b4505f62368187ce044f3b0511
Sstark97 <aitorscinfo@gmail.com>
2022-12-27 20:12:19 +0000

    Add moment to format date

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,16 +1,21 @@
+import moment from "moment"
 import type { Movie } from "@customTypes/movies"
 import { IMG_URI } from "../const"
 import "@styles/components/MovieItem.scss"
 
-const MovieItem = ({ movie }: {movie: Movie}) => {
+const MovieItem = ({ movie }: { movie: Movie }) => {
+  const { poster_path, original_title, release_date, popularity } = movie
+
+  // Format Date with moment.js and in English Format
+  const formatDate = moment(release_date).format("ll")
   return (
     <li>
-        <img src={`${IMG_URI}${movie.poster_path}`} alt="poster" />
-        <div className="movie_info">
-            <h3>{movie.original_title}</h3>
-            <p>{movie.release_date}</p>
-            <p>{movie.popularity}</p>
-        </div>
+      <img src={`${IMG_URI}${poster_path}`} alt="poster" />
+      <div className="movie_info">
+        <h3>{original_title}</h3>
+        <p>{formatDate}</p>
+        <p>{popularity}</p>
+      </div>
     </li>
   )
 }
