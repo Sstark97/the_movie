92a584b9fa8b78c70772e26888aeaa1643bec40b
Sstark97 <aitorscinfo@gmail.com>
2022-12-27 16:37:40 +0000

    Add root styles

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,16 +1,17 @@
 import Header from "@components/Header"
 import Footer from "@components/Footer"
 import type { Children } from "@customTypes/globals"
+import "@styles/containers/Layout.scss"
 
 const Layout = ({ children }: Children) => {
   return (
-    <>
+    <div className="app_container">
         <Header/>
         <main>
             { children }
         </main>
         <Footer />
-    </>
+    </div>
   )
 }
 
