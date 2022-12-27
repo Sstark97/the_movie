a1fe152a7de739f60f891dc3fee1dcf3c41bc940
Sstark97 <aitorscinfo@gmail.com>
2022-12-27 19:41:16 +0000

    Add IMG_URI

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,9 +1,10 @@
 import type { Movie } from "@customTypes/movies"
+import { IMG_URI } from "../const"
 
 const MovieItem = ({ movie }: {movie: Movie}) => {
   return (
     <li>
-        <p>{movie.original_title}</p>
+        <img src={`${IMG_URI}${movie.poster_path}`} alt="poster" />
     </li>
   )
 }
--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,7 @@
 const API_MOST_POPULAR = `${import.meta.env.VITE_API}movie/popular?api_key=${import.meta.env.VITE_API_KEY_AUTH_V3}`
+const IMG_URI = "https://image.tmdb.org/t/p/w220_and_h330_face/"
 
 export {
-    API_MOST_POPULAR
+    API_MOST_POPULAR,
+    IMG_URI
 }
