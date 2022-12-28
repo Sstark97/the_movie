d4ad7f549c046480bfa2a1846e0a84b3c7234c29
Sstark97 <aitorscinfo@gmail.com>
2022-12-28 22:07:34 +0000

    Finish MovieDetails

--- src/hooks/useProvideMovies.ts
+++ src/hooks/useProvideMovies.ts
@@ -4,6 +4,7 @@ import type { FailResponse, Response, Movie } from "@customTypes/movies"
 
 const useProvideMovies = () => {
     const [movies, setMovies] = useState<Movie[]>([] as Movie[])
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [page, setPage] = useState(1)
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<string>("")
