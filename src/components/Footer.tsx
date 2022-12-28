8df929414e26460e770ca556fc1db006cce5cab1
Sstark97 <aitorscinfo@gmail.com>
2022-12-28 11:18:08 +0000

    Finish responsive in Movie Components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "@styles/components/Footer.scss"
 const Footer = () => {
   return (
     <footer>
-      <p>© Made by Aitor Santana Cabrera for a React Testing (2coders)</p>
+      <p>© Made by Aitor Santana Cabrera for a React Testing</p>
     </footer>
   )
 }
