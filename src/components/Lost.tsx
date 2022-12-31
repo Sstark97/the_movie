0133e72331a981183aeb2233032305056fff74ec
Sstark97 <aitorscinfo@gmail.com>
2022-12-31 10:51:20 +0000

    Add Lost Component

---
+++ src/components/Lost.tsx
@@ -0,0 +1,15 @@
+import "@styles/components/Lost.scss"
+
+const Lost = () => {
+  return (
+    <div id="not_found_container">
+      <div id="eyes">
+        <div></div>
+        <div></div>
+      </div>
+      <div id="not_found"></div>
+    </div>
+  )
+}
+
+export default Lost
